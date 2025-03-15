6a674ef23396e9abc6b9401e4cb6c5fc5a326922
✪Ohmcodes™✪ <ohmcodes@gmail.com>
2025-03-14 23:32:18 -0700

    updates

--- Enshrouded.cs/Enshrouded.cs
+++ Enshrouded.cs/Enshrouded.cs
@@ -77,29 +77,34 @@ namespace WindowsGSM.Plugins
         public async void CreateServerCFG()
         {
             GeneratePasswords();
-
+            
             var serverConfig = new
             {
                 name = $"{_serverData.ServerName}",
-                password = "",
                 saveDirectory = "./savegame",
                 logDirectory = "./logs",
                 ip = $"{_serverData.ServerIP}",
                 gamePort = Int32.Parse(_serverData.ServerPort),
                 queryPort = Int32.Parse(_serverData.ServerQueryPort),
                 slotCount = Int32.Parse(_serverData.ServerMaxPlayer),
+                voiceChatMode = "Proximity",
+                enableVoiceChat = false,
+                enableTextChat = false,
                 gameSettingsPreset = "Default",
                 gameSettings = new
                 {
                     playerHealthFactor = 1,
                     playerManaFactor = 1,
                     playerStaminaFactor = 1,
+                    playerBodyHeatFactor = 1,
                     enableDurability = true,
                     enableStarvingDebuff = false,
                     foodBuffDurationFactor = 1,
                     fromHungerToStarving = 600000000000,
                     shroudTimeFactor = 1,
                     tombstoneMode = "AddBackpackMaterials",
+                    enableGliderTurbulences = true,							   
+                    weatherFrequency = "Normal",
                     miningDamageFactor = 1,
                     plantGrowthSpeedFactor = 1,
                     resourceDropStackAmountFactor = 1,
@@ -119,6 +124,7 @@ namespace WindowsGSM.Plugins
                     bossHealthFactor = 1,
                     threatBonus = 1,
                     pacifyAllEnemies = false,
+                    tamingStartleRepercussion = "LoseSomeProgress",
                     dayTimeDuration = 1800000000000,
                     nightTimeDuration = 720000000000
                 },
